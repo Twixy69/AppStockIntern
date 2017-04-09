6114c8067f94672a6c287a6250e6e17529dba31c
Twixy69 <laurent.cetin@telecom-sudparis.eu>
2017-04-09 22:31:14 +0200

    bl CRUD ok

--- app/Http/Controllers/BLController.php
+++ app/Http/Controllers/BLController.php
@@ -44,9 +44,9 @@ class BLController extends Controller
      */
     public function store(Request $request)
     {
-      $colis_s = new Colis($request->all());
-      $colis_s->save();
-      return redirect()->route('colis.index');
+      $b_ls = new BL($request->all());
+      $b_ls->save();
+      return redirect()->route('b_l.index');
     }
 
     /**
@@ -55,9 +55,11 @@ class BLController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(BL $bl)
+    public function show($b_l)
     {
-        //
+        $b_ls = new BL;
+        $b_ls = BL::find($b_l);
+        return view('b_ls/show',compact('b_ls'));
     }
 
     /**
@@ -66,9 +68,12 @@ class BLController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(BL $bl)
+    public function edit($b_l)
     {
-        //
+        $b_ls = new BL;
+        $b_ls = BL::find($b_l);
+        $idAdress = Adresse::pluck('name','id');
+        return view('b_ls/edit',compact('b_ls','idAdress'));
     }
 
     /**
@@ -78,9 +83,15 @@ class BLController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, BL $bl)
+    public function update($b_l, Request $request)
     {
-        //
+            $b_ls = new BL;
+            $b_ls = BL::find($b_l);
+
+            $b_ls->update($request->all());
+            $b_ls->save;
+
+            return redirect()->route('b_l.edit', [$b_ls]);
     }
 
     /**
@@ -89,8 +100,10 @@ class BLController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(BL $bl)
+    public function destroy($b_l)
     {
-        //
+        BL::destroy($b_l);
+
+        return redirect()->route('b_l.index');
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,97 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+class DatabaseSeeder extends Seeder {
+
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      $this->call('UserTableSeeder');
+      $this->command->info('User table seeded!');
+
+      $this->call('AdressTableSeeder');
+      $this->command->info('Adress table seeded!');
+
+      $this->call('AffaireTableSeeder');
+      $this->command->info('Affaire table seeded!');
+
+      $this->call('BLTableSeeder');
+      $this->command->info('BL table seeded!');
+
+      $this->call('ColisTableSeeder');
+      $this->command->info('Colis table seeded!');
+
+      $this->call('PieceTableSeeder');
+      $this->command->info('Piece table seeded!');
+
+      /*$this->call('RelColPieTableSeeder');
+      $this->command->info('RelColPie table seeded!');*/
+
+    }
+
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+      DB::table('users')->delete();
+      DB::table('users')->insert([
+        'name' => 'User_1',
+        'email'=> 'user1@user.com',
+        'password' => bcrypt('azerty')
+        ]);
+
+    }
+}
+
+class AdressTableSeeder extends Seeder {
+
+    public function run()
+    {
+      //DB::table('users')->delete();
+
+      //User::create(['email' => 'foo@bar.com']);
+    }
+}
+
+class AffaireTableSeeder extends Seeder {
+
+    public function run()
+    {
+      //DB::table('users')->delete();
+
+      //User::create(['email' => 'foo@bar.com']);
+    }
+}
+
+class BLTableSeeder extends Seeder {
+
+    public function run()
+    {
+      //DB::table('users')->delete();
+
+      //User::create(['email' => 'foo@bar.com']);
+    }
+}
+
+class ColisTableSeeder extends Seeder {
+
+    public function run()
+    {
+      //DB::table('users')->delete();
+
+      //User::create(['email' => 'foo@bar.com']);
+    }
+}
+
+class PieceTableSeeder extends Seeder {
+
+    public function run()
+    {
+      //DB::table('users')->delete();
+
+      //User::create(['email' => 'foo@bar.com']);
     }
 }
